63e76ef3654cd422e6a214fb5771445c106bcb43
Madhu Kumar <madhukumar.gowda@gmail.com>
2024-01-31 19:59:17 +0530

    File System in NodeJS Synchronous Process

---
+++ filereadwrite.js
@@ -0,0 +1,11 @@
+// this program is to demonstrate the read and write file operation synchronously
+
+const fs = require('fs');
+
+// this method has perfromance issue if file has more data.
+let textin = fs.readFileSync('./Files/input.txt', 'utf-8');
+console.log(textin);
+
+let outputTxt = `Text from input text : ${textin} + \n Date Created ${new Date()}`; 
+// If file is not preasent then node will create output file then write text to file.
+fs.writeFileSync("./Files/output.txt", outputTxt);
